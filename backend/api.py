fcdb228c4ab5358a81569e7af7ce7ee6015d01ee
isaacsquires <isaacsquires@me.com>
2022-01-13 15:45:23 +0000

    add resource index flask

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api, reqparse
 from model.evaluate import evaluate
 from model.train import train
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+
+app._static_folder = os.path.abspath("templates/static")
 
 
 class EvaluateModel(Resource):
@@ -25,8 +28,15 @@ class TrainModel(Resource):
         return {'modelTrained': True}
 
 
+class RenderIndex(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'), 200, headers)
+
+
 api.add_resource(EvaluateModel, '/eval')
 api.add_resource(TrainModel, '/train')
+api.add_resource(RenderIndex, '/')
 
 if __name__ == '__main__':
     # app.run(debug=True)
